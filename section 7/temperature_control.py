f07cdd8d980834646abf2dabaf510a18405a18a2
Fkureski <felipekureski@gmail.com>
2025-03-17 15:33:29 -0300

    Easy exercises

---
+++ section 7/temperature_control.py
@@ -0,0 +1,10 @@
+temperature = int(input('Digite a temperatura:'))
+
+if (temperature < 10):
+    print('It is very cold')
+
+elif (temperature < 20):
+    print('It is warm')
+
+else: 
+    print('It is very hot')
---
+++ section 7/welcoming.py
@@ -0,0 +1,10 @@
+hour = int(input('Digite o horario:'))
+
+if (hour < 12):
+    print('Good morning!')
+
+elif (hour < 18):
+    print('Good evening!')
+
+else: 
+    print('Good night!')
